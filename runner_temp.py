4c4dc6c2fee22850c423b2294a1a9908471bb61d
Ly <lyttt.dev@gmail.com>
2022-08-23 19:41:13 +0700

    feat: add json bridge

---
+++ runner_temp.py
@@ -0,0 +1,21 @@
+# read result from file json
+# startColorize(input, output, render_factor)
+import json
+from runner import startColorize
+
+input = ""
+output = ""
+render = ""
+json_path = "userVideo.json"
+
+print("Start reading json file...")
+with open(json_path) as f:
+    data = json.load(f)
+    print(data["input_video"])
+    print(data["output_video"])
+    print(data["render_factor"])
+    input = data["input_video"]
+    output = data["output_video"]
+    render = data["render_factor"]
+
+startColorize(input_paths=[input], output_paths=[output], render_factor=render)
